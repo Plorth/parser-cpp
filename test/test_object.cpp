fb2620342ef51644e494f5f9a1b3fc190d176ca3
Rauli Laine <rauli.laine@iki.fi>
2020-06-19 16:32:16 +0300

    Add test cases for object parsing
    Introduce test cases for parsing objects.

---
+++ test/test_object.cpp
@@ -0,0 +1,117 @@
+#include <cassert>
+#include <cstdlib>
+
+#include <plorth/parser.hpp>
+
+using plorth::parser::parse_object;
+
+static auto parse(const std::u32string& source)
+{
+  auto begin = std::cbegin(source);
+  const auto end = std::cend(source);
+  plorth::parser::position position;
+
+  return parse_object<std::u32string::const_iterator>(begin, end, position);
+}
+
+static void test_eof_before_the_object()
+{
+  const auto result = parse(U"");
+
+  assert(!result);
+}
+
+static void test_no_brace_found()
+{
+  const auto result = parse(U"invalid");
+
+  assert(!result);
+}
+
+static void test_unterminated_object()
+{
+  const auto result = parse(U"{");
+
+  assert(!result);
+}
+
+static void test_unterminated_object_after_key()
+{
+  const auto result = parse(U"{\"foo\":");
+
+  assert(!result);
+}
+
+static void test_unterminated_object_after_property()
+{
+  const auto result = parse(U"{\"foo\": \"bar\"");
+
+  assert(!result);
+}
+
+static void test_unterminated_object_after_comma()
+{
+  const auto result = parse(U"{\"foo\": \"bar\",");
+
+  assert(!result);
+}
+
+static void test_properties_without_comma()
+{
+  const auto result = parse(U"{\"foo\": \"bar\" \"baz\": \"quux\"}");
+
+  assert(!result);
+}
+
+static void test_empty_object()
+{
+  const auto result = parse(U"{}");
+
+  assert(!!result);
+  assert((*result.value())->properties().size() == 0);
+}
+
+static void test_object_with_one_property()
+{
+  const auto result = parse(U"{\"foo\": \"bar\"}");
+
+  assert(!!result);
+  assert((*result.value())->properties().size() == 1);
+  assert((*result.value())->properties()[0].first == U"foo");
+}
+
+static void test_object_with_multiple_properties()
+{
+  const auto result = parse(U"{\"foo\": \"bar\", \"baz\": \"quux\"}");
+
+  assert(!!result);
+  assert((*result.value())->properties().size() == 2);
+  assert((*result.value())->properties()[0].first == U"foo");
+  assert((*result.value())->properties()[1].first == U"baz");
+}
+
+static void test_object_with_multiple_properties_with_dangling_comma()
+{
+  const auto result = parse(U"{\"foo\": \"bar\", \"baz\": \"quux\",}");
+
+  assert(!!result);
+  assert((*result.value())->properties().size() == 2);
+}
+
+int main(int argc, char** argv)
+{
+  test_eof_before_the_object();
+  test_no_brace_found();
+  test_unterminated_object();
+  test_unterminated_object_after_key();
+  test_unterminated_object_after_property();
+  test_unterminated_object_after_comma();
+  test_properties_without_comma();
+
+  test_empty_object();
+  test_object_with_one_property();
+  test_object_with_multiple_properties();
+  test_object_with_multiple_properties_with_dangling_comma();
+
+  return EXIT_SUCCESS;
+}
