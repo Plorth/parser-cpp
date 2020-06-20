73ebff8a6e593cd3038a77570f993b9b24c747de
Rauli Laine <rauli.laine@iki.fi>
2020-06-20 13:05:06 +0300

    Add test cases for parsing symbols
    Introduce test cases for parsing symbols.

---
+++ test/test_symbol.cpp
@@ -0,0 +1,46 @@
+#include <cassert>
+#include <cstdlib>
+
+#include <plorth/parser.hpp>
+
+using plorth::parser::parse_symbol;
+
+static auto parse(const std::u32string& source)
+{
+  auto begin = std::cbegin(source);
+  const auto end = std::cend(source);
+  plorth::parser::position position;
+
+  return parse_symbol<std::u32string::const_iterator>(begin, end, position);
+}
+
+static void test_eof_before_the_symbol()
+{
+  const auto result = parse(U"");
+
+  assert(!result);
+}
+
+static void test_non_symbol()
+{
+  const auto result = parse(U"[foo]");
+
+  assert(!result);
+}
+
+static void test_symbol()
+{
+  const auto result = parse(U"foo-bar-baz");
+
+  assert(!!result);
+  assert((*result.value())->id() == U"foo-bar-baz");
+}
+
+int main(int argc, char** argv)
+{
+  test_eof_before_the_symbol();
+  test_non_symbol();
+  test_symbol();
+
+  return EXIT_SUCCESS;
+}
