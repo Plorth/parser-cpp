70ecddfe8815d846f65e2931c2bb7cafa487f759
Rauli Laine <rauli.laine@iki.fi>
2022-10-02 22:31:23 +0300

    Add another test case

---
+++ test/test_hello_world.cpp
@@ -0,0 +1,20 @@
+#include <cassert>
+#include <cstdlib>
+
+#include <plorth/parser.hpp>
+
+int main(int argc, char** argv)
+{
+  const std::u32string source(U"'Hello, World!' println");
+  auto begin = std::cbegin(source);
+  const auto end = std::cend(source);
+  plorth::parser::position position;
+  const auto result = plorth::parser::parse(begin, end, position);
+
+  assert(!!result);
+  assert(result.value()->size() == 2);
+  assert(result.value()->at(0)->type() == plorth::parser::ast::token::type::string);
+  assert(result.value()->at(1)->type() == plorth::parser::ast::token::type::symbol);
+
+  return EXIT_SUCCESS;
+}
